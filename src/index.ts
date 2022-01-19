6d19f6d84861e6de979034525f81fae28522010f
Abdulrahman <abdul.ashref333@gmail.com>
2022-01-19 09:09:38 +0200

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import routes from "./routes/index";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Connected!");
-});
+// routes
+app.use("/api", routes);
 
 app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}/`);
+  console.log(`server is running on http://localhost:${port}/api/`);
 });
